a37eb1aa18fdd22d9e0e6538aaf2437152b88c1c
Mario Torrisi <torrisi.mario@gmail.com>
2016-04-03 11:15:08 +0100

    Minor fix in run.sh

--- liferay-6.1.1-bundle/run.sh
+++ liferay-6.1.1-bundle/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 set -e
-
-asadmin start-domain
-if [ ! -f /.glassfish_admin_password_changed ]; then
-    /change_admin_password.sh
-fi
-echo "=> Restarting Glassfish server"
-asadmin stop-domain
 echo "=> Starting and running Glassfish server"
-asadmin start-domain & > asadmin.out 2>&1
-tail -f  /opt/liferay-portal-6.1.1-ce-ga2/glassfish-3.1.2/domains/domain1/logs/server.log
+if [ ! -f /.glassfish_admin_password_changed ]; then
+  echo "=> Performing first time starting"
+  asadmin start-domain
+  /change_admin_password.sh
+  echo "=> Restarting Glassfish server"
+  asadmin stop-domain
+fi
+asadmin start-domain --verbose
